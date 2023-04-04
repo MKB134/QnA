55cafd12fa6c85470bd925a85593aec301bffbb2
MKB134 <murad1110488@gmail.com>
2023-04-04 13:28:28 +0300

    create tests for questions and answers

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:question)  }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :body }
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:answers) }
+  end
+
+  describe 'validation' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }
+  end
 end
