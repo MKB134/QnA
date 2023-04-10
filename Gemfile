911ba21fcf7e6d26924ff7aa4901ab94bcb8786e
MKB134 <murad1110488@gmail.com>
2023-04-10 11:40:09 +0300

    add_questions_spec

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
+gem 'slim-rails'
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,15 @@ module Qna
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # config.eager_load_paths << Rails.root.join("extras").
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false,
+                       controller_specs: true
+    end
   end
 end
