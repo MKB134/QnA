58c1e6958910370d6a27bf1aa3cb2ae5fccb8583
MKB134 <murad1110488@gmail.com>
2023-04-17 16:50:06 +0300

    show answer spec

---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature 'Пользователь может просматривать вопрос и ответы к нему', %q{
+  Чтобы получить ответы
+  Любой пользователь
+  Может просматривать вопрос и ответы к нему
+} do
+  given(:question) { create(:question) }
+
+  background do
+    question.answers.create!(body: 'answer 1')
+    question.answers.create!(body: 'answer 2')
+  end
+
+  scenario 'Пользователь просматривает Вопрос и ответы к нему' do
+    visit question_path(question)
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+    expect(page).to have_content 'Answers'
+    expect(page).to have_content 'answer 1'
+    expect(page).to have_content 'answer 2'
+  end
+end
